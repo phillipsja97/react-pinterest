9c0d55d6b5b10e94d1082d64ea9f065bb5d884db
Jamie <phillipsja97@gmail.com>
2019-12-16 20:41:35 -0600

    add boards is working

--- src/Components/BoardsContainer/BoardsContainer.js
+++ src/Components/BoardsContainer/BoardsContainer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import boardData from '../../helpers/data/boardData';
 import authData from '../../helpers/data/authData';
 import Board from '../Board/Board';
-import PropTypes from 'prop-types';
+import BoardForm from '../BoardForm/BoardForm';
 
 class BoardsContainer extends React.Component {
   static propTypes = {
@@ -14,6 +15,10 @@ class BoardsContainer extends React.Component {
   }
 
   componentDidMount() {
+    this.getBoards();
+  }
+
+  getBoards = () => {
     boardData.getBoardsByUid(authData.getUid())
       .then((boards) => {
         this.setState({ boards });
@@ -21,10 +26,20 @@ class BoardsContainer extends React.Component {
       .catch((error) => console.error(error));
   }
 
+
+  addBoard = (newBoard) => {
+    boardData.saveBoard(newBoard)
+      .then(() => {
+        this.getBoards();
+      })
+      .catch((errorFromSaveBoard) => console.error(errorFromSaveBoard));
+  }
+
   render() {
-    const { setSingleBoard } = this.props;
+    const { setSingleBoard, addBoard } = this.props;
     return (
     <div className="d-flex flex-wrap justify-content-center">
+      <BoardForm addBoard={this.addBoard}/>
       {this.state.boards.map((board) => (<Board key={board.id} board={board} setSingleBoard={setSingleBoard} />))}
     </div>
     );
